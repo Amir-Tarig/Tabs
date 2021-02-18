786e1c5fc1cbf6fad0fcc1e0f1d1be860eb10915
Miro <MiroPhoenix91@gmail.com>
2021-02-18 16:23:15 +0100

    Tabs

--- app.js
+++ app.js
@@ -1,29 +1,17 @@
-function openCity(){
+function openCity(eve, className){
     let tabcontent = [...document.querySelectorAll('.tabcontent')]
     let tablinks = [...document.querySelectorAll('.tablinks')]
-    let conId = []
+    
 
     tabcontent.map(con => {
         con.style.display = "none"
-        conId.push(con.id)
-    })
-    tablinks.map((btn,i)=> {
         
-        btn.addEventListener('click', function(){
-          
-           conId.map(id => {
-            if(this.textContent === id){
-               let city = document.getElementById(id)
-               city.style.display = 'none'
-               btn.className = btn.className.replace("active", "")
-               city.style.display = 'block'
-               btn.classList.add('active')
-            }
-           })
-        })
+    })
+    tablinks.map((btn)=> {   
+       btn.className = btn.className.replace(' active', "")
     })
 
-    // console.log(conId)
+    document.getElementById(className).style.display = "block"
+    eve.currentTarget.className += ' active'
 }
 
-openCity()
